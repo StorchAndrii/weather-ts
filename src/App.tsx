b254f7c63c728685e76a01e92b619c38c374ab64
StorchAndrii <andrey.st.light@gmail.com>
2022-05-01 01:43:09 +0300

    add Structur add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { MonthStatistics } from "./pages/MonthStatistics/MonthStatistics";
+import { Header } from "./sharedHeader/Header";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/month-statistics" element={<MonthStatistics />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/sharedHeader/Header.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Select from "react-select";
+import s from "./Header.module.scss";
+import { GlobalSvgSelector } from "../assets/images/icons/global/GlobalSvgSelector";
+
+interface Props {}
+
+export const Header = (props: Props) => {
+  const options = [
+    { value: "city-1", label: "Kyiv" },
+    { value: "sity-2", label: "Odessa" },
+    { value: "sity-3", label: "Lviv" },
+  ];
+
+  const colourStyles = {
+    control: (styles: any) => ({
+      ...styles,
+      backgroundColor: "rgba(71, 147, 255, 0.2)",
+      width: "194px",
+      height: "37px",
+      borderRadius: "10px",
+      zIndex: 100,
+    }),
+  };
+
+  return (
+    <header className={s.header}>
+      <div className={s.wrapper}>
+        <div className={s.logo}>
+          <GlobalSvgSelector id={"header-logo"} />
+        </div>
+        <div className={s.title}>React weather</div>
+      </div>
+      <div className={s.wrapper}>
+        <div className={s.change_theme}>
+          <GlobalSvgSelector id={"change_theme"} />
+        </div>
+        <Select
+          defaultValue={options[0]}
+          styles={colourStyles}
+          options={options}
+        />
+      </div>
+    </header>
+  );
+};
